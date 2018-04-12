b0f3e453b05c93129c21ae2b1a6fe512c65ef863
Jony635 <jony635molina@gmail.com>
2018-04-12 18:57:44 +0200

    BossHpBar implemented

---
+++ Warcraft Heroes Beyond Time/Warcraft Heroes Beyond Time/BossHPBar.h
@@ -0,0 +1,27 @@
+#ifndef __BOSSHPBAR_H__
+#define __BOSSHPBAR_H__
+
+#include "GUIElem.h"
+
+class BossEntity;
+
+class BossHPBar : public GUIElem
+{
+public:
+
+	BossHPBar() {}
+	BossHPBar(BossEntity* boss, fPoint localPos, GUIElem* parent = nullptr);
+	virtual ~BossHPBar();
+
+	bool Update(float dt);
+	bool Draw();
+
+private:
+	BossEntity* boss = nullptr;
+	SDL_Rect actualrect = { 301, 841, 310, 8 };
+
+};
+
+
+
+#endif
