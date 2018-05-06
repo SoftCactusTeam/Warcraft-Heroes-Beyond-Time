85b034ec18a734dde0a3b92317743cd165e70175
morgadoCV <arnau.morgado@gmail.com>
2018-05-07 00:15:52 +0200

    added the new class

---
+++ Warcraft Heroes Beyond Time/Warcraft Heroes Beyond Time/LifeStealItem.cpp
@@ -0,0 +1,32 @@
+#include "LifeStealItem.h"
+
+
+
+LifeStealItem::LifeStealItem()
+{
+}
+
+
+LifeStealItem::~LifeStealItem()
+{
+}
+
+bool LifeStealItem::Start()
+{
+	return false;
+}
+
+bool LifeStealItem::Act(ModuleItems::ItemEvent event, float dt)
+{
+	return false;
+}
+
+bool LifeStealItem::Draw()
+{
+	return false;
+}
+
+bool LifeStealItem::printIconOnScreen(iPoint pos)
+{
+	return false;
+}
