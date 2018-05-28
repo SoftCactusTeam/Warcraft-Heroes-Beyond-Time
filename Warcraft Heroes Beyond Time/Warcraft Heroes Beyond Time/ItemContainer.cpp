f5fe2f2ec8e38d3622a2d841d5d911e92cd5dd6c
Jony635 <jony635molina@gmail.com>
2018-05-28 19:57:24 +0200

    Picking items with 1 click

--- Warcraft Heroes Beyond Time/Warcraft Heroes Beyond Time/ItemContainer.cpp
+++ Warcraft Heroes Beyond Time/Warcraft Heroes Beyond Time/ItemContainer.cpp
@@ -43,9 +43,9 @@ bool ItemContainer::Update(float dt)
 			anim = &focused_anim;
 		if ((App->input->GetPadButtonDown(SDL_CONTROLLER_BUTTON_A) == KeyState::KEY_DOWN || App->input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_DOWN) && item != nullptr)
 		{
-			if (!selected)
+			/*if (!selected)
 				selected = true;
-			else
+			else*/
 				anim = &select_anim;
 		}
 		else if (anim == &select_anim && select_anim.Finished())
